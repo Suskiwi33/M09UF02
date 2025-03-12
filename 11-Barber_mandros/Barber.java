66baa29e9d941c9592d679bd1034c90aa4d82d28
alex <alexvictorb@gmail.com>
2025-03-12 18:27:56 +0100

    11-Barber_mandros totalment acabat i funcional

--- 11-Barber_mandros/Barber.java
+++ 11-Barber_mandros/Barber.java
@@ -1,14 +1,40 @@
+import java.util.Random;
+
 public class Barber extends Thread{
     private String nom;
+    private Barberia barberia = Barberia.barberia;
+    private Object condBarber = barberia.condBarber;
+    private Random rnd;
 
     public Barber (String nom){
         this.nom = nom;
+        this.rnd = new Random();
     }
 
     @Override
     public void run (){
         while(true){
             
+            Client client = barberia.seguentClient();
+
+            if (client!=null) {
+                System.out.println("Li toca al client: " + client.getNom());
+
+                long extra = rnd.nextInt(100);
+                try {
+                    Thread.sleep(900 + extra);
+                } catch (InterruptedException e) {}
+
+                client.tallarseElCabell();
+            } else {
+                System.out.println("Ningú en espera");
+                System.out.println("Barber " + this.nom + " dormint.");
+
+                synchronized(condBarber) {
+                    try { condBarber.wait(); }
+                    catch (InterruptedException e) {}
+                }
+            }
         }
     }
 }
--- 11-Barber_mandros/Barberia.java
+++ 11-Barber_mandros/Barberia.java
@@ -3,12 +3,14 @@ import java.util.Queue;
 import java.util.Random;
 
 public class Barberia extends Thread{
+
+    public static Barberia barberia;
     
-    private Queue cua;
+    private Queue<Client> cua;
     private int maxCadires;
     private int numCua;
     private Random rnd;
-    private final Object lock1 = new Object();
+    public final Object condBarber = new Object();
 
 
     public Barberia(int numCad){
@@ -16,73 +18,55 @@ public class Barberia extends Thread{
         this.cua = new LinkedList<Client>();
         this.numCua=1;
         this.rnd = new Random();
-        for(int i = 1; i<21;i++){
-            cua.add(new Client(i));
-        }
+        
     }
 
-    public Client seguentClient(){
-        if(numCua !=21){
-            System.out.println("Li toca al client " + ((LinkedList<Client>) this.cua).get(numCua).getNom());
-            return ((LinkedList<Client>) this.cua).get(numCua);
-
-        }else{
-
-            return null;
-        }
-
+    
+    public Client seguentClient() {
+        return cua.poll();
     }
 
-    public void entrarClient(Client client){
-        if(maxCadires != 0){
-            System.out.println("Client " + client.getNom() + " en espera.");
-            maxCadires -=1;
-        }else{
-            System.out.println("No queden cadires, client " + client.getNom() + " se'n va.");
+    public void entrarClient(Client client) {
+        if (cua.size() < maxCadires) { 
+            cua.add(client);
+            System.out.println("Client " + client.getNom() + " en espera");
+    
+            synchronized (condBarber) {
+                condBarber.notifyAll(); 
+            }
+        } else {
+            System.out.println("No queden cadires, client " + client.getNom() + " se'n va");
         }
     }
+    
+
 
     @Override
     public void run() {
         
-        for(int i = 0; i<10;i++){
-            entrarClient(seguentClient());
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                
-                e.printStackTrace();
-            }
-            numCua++;
-        }
+        int id = 1;
+        for(int j=0; j<2; j++) {
+            for(int i=0; i<10; i++) {
+                entrarClient(new Client(id));
+                try { Thread.sleep(500); }
+                catch (InterruptedException e) {}
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            
-            e.printStackTrace();
-        }
-        
-        for(int y = 0; y<10;y++){
-            entrarClient(seguentClient());
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                
-                e.printStackTrace();
+                id++;
             }
-            numCua++;
+            
+            try { Thread.sleep(10000); }
+            catch (InterruptedException e) {}
         }
 
     }
 
     public static void main(String[] args) {
 
-        Barberia bar = new Barberia(3);
+        barberia = new Barberia(3);
         Barber barber = new Barber("XD");
 
         barber.start();
-        bar.start();
+        barberia.start();
 
     }
 
